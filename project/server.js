1146b5f4239fec0fb5b307f92f40dd63bdf3f37e
Sola Lhim <131292558+pooloom69@users.noreply.github.com>
2024-07-23 21:22:42 -0700

    Update server.js

--- project/server.js
+++ project/server.js
@@ -1,40 +1,52 @@
-const express = require('express');
-const axios = require('axios'); 
-const cheerio = require('cheerio');
-const app = express();
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
+// Importing libraries and modules
+const express = require('express'); // handle HTTP requests
+const axios = require('axios');  // making HTTP requests to external services
+const cheerio = require('cheerio'); // parsing and manipulating HTML
+const ejs = require('ejs') 
+const bodyParser = require('body-parser') // parsing incoming request bodies
+const cors = require('cors'); // integrating applications 
+const mysql = require('mysql2/promise'); 
+const fs = require('fs'); // handle file operations
+const csvParser = require('csv-parser'); 
+require('dotenv').config() 
 
-const cors = require('cors'); 
-const mysql = require('mysql2/promise');
-const fs = require('fs'); 
-const csvParser = require('csv-parser');
+const app = express();  
 
-const port = process.env.PORT || 3000;
-app.use(cors());
-app.use(express.json());
-require('dotenv').config()
+// Server configuration
+const port = process.env.PORT || 3001;  
+app.use(cors()); 
+app.use(express.json()); 
+app.use(bodyParser.urlencoded({ extended: false })) 
 
+// Setting up path and views for EJS
 const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({ extended: false }))
+app.set('views', path.join(__dirname, 'views')); // set the directory where views are stored
+app.use(express.static(path.join(__dirname, 'public'))); // serve static files from the public directory
+app.set('view engine','ejs') // set EJS as template engine
 
-const csvFilePath = process.env.CSV_PATH || path.join(__dirname, 'courseCode.csv');
-const fileStream = fs.createReadStream(csvFilePath);
+// CSV file path configuration and stream creation
+//const csvFilePath = process.env.CSV_PATH || path.join(__dirname, 'courseCode.csv'); // define the CSV file path
+const csvFilePath = path.join(__dirname, 'courseCode.csv');
+const fileStream = fs.createReadStream(csvFilePath); // create a readable stream from the CSV file  (read chunk of file not whole memory effective!)
+
+const golfFinderApiKey = process.env.golfFinderApiKey;
+const geocodingApiKey = process.env.geocodingApiKey;
+
 
 fileStream.on('error', (err) => {
-    console.error('Error reading the CSV file:', err.message);
+    console.error('Error reading the CSV file:', err.message); // error handling for file stream
 });
 
+
+// Database configuration
 const db = {
-    database: "golfcourse_data", // default to "golfcourse_data" if not set
+    database: "golfcourse_data", 
     host: process.env.STACKHERO_MARIADB_HOST || "127.0.0.1",
     user: "root",
-    password: process.env.STACKHERO_MARIADB_ROOT_PASSWORD || "thfdk69", // Please remove this default password for security reasons
+    password: process.env.STACKHERO_MARIADB_ROOT_PASSWORD || "thfdk69", 
     port: 3306
 };
+
 
 // Create a pool to manage connections to the database
 const pool = mysql.createPool(db);
@@ -56,28 +68,22 @@ async function golfCourseExists(courseName) {
     }
 }
 
-
-//inserts a new golf course into the database. It first checks if the course already exists, and if not, 
-//it inserts the course ratings and slope ratings into the database.
+//inserts a new golf course into the database. 
 async function insertGolfCourseData(courseInfo) {
     if (!courseInfo || !courseInfo.Golfcourse_Name) {
         console.error("Invalid course info provided.");
         return;
     }
-
     // Check if golf course already exists
     const exists = await golfCourseExists(courseInfo.Golfcourse_Name);
     if (exists) {
         console.log(`Golf course "${courseInfo.Golfcourse_Name}" already exists. Skipping insertion.`);
         return;
     }
-
     let connection;
     try {
         // Establish a database connection from the pool
         connection = await pool.getConnection();
-
-        // Begin a transaction
         await connection.beginTransaction();
         
         // Insert the golf course information into the database
@@ -110,6 +116,7 @@ async function insertGolfCourseData(courseInfo) {
     }
 }
 
+
 let existingCourseNames = new Set();
 
 // Fetch all existing course names and store in the Set
@@ -126,18 +133,17 @@ async function fetchExistingCourses() {
 }
 
 
-
-// fetch the HTML content from url 
+// Fetch the HTML content from the URL
 const getHTML = async (keyword) => { 
     try {
-        const html = await axios.get(`https://ncrdb.usga.org/courseTeeInfo.aspx?CourseID=${keyword}`);
-        return html.data;
+        const response = await axios.get(`https://ncrdb.usga.org/courseTeeInfo?CourseID=${keyword}`);
+        console.log(response);
+        return response.data;
     } catch (e) {
-        console.error("Error making Axios request:", e.response.status, e.response.statusText);
+        console.error("Error making Axios request:", e.response?.status, e.response?.statusText);
         return null;
     }
 };
-
 
 
 // Function to get golf course info from the USGA website 
@@ -147,66 +153,62 @@ async function getGolfCourseInfo(keyword) {
         console.error("Failed to fetch HTML data from the website.");
         return null;
     }
-    try{
+    try {
         const $ = cheerio.load(html);
-
-        const golfCourseName = $("#gvCourseTees > tbody > tr:nth-child(2) > td:nth-child(1)").text().trim();
+        const golfCourseName = $("#gvCourseTees > tbody > tr:nth-child(2) > td.tableCellWidth").text().trim();
 
         // Extract the tee names, course ratings, and slope ratings separately
         const teeNames = [];
         const courseRatings = [];
         const slopeRatings = [];
-        for (let i = 2; i <= 26; i++) {
-            const teeName = $(`#gvTee > tbody > tr:nth-child(${i}) > td:nth-child(1)`).text().trim();
-            const courseRating = parseFloat($(`#gvTee tr:nth-child(${i}) td:nth-child(4)`).text().trim());
-            const slopeRating = parseFloat($(`#gvTee > tbody > tr:nth-child(${i}) > td.GVPadding.dtPadding.ratingsStyleItems`).text().trim());
+        
+        $("#gvTee > tbody > tr").each((index, element) => {
+            const teeName = $(element).find("td:nth-child(1)").text().trim();
+            const courseRating = parseFloat($(element).find("td:nth-child(4)").text().trim());
+            const slopeRating = parseFloat($(element).find("td:nth-child(5)").text().trim());
 
             // Handle NaN values or parsing errors
-        if (isNaN(courseRating)) {
-            courseRatings.push(0); // Use a default value of 0 for invalid ratings
-        } else {
-            courseRatings.push(courseRating);
-        }
-
-        if (isNaN(slopeRating)) {
-            slopeRatings.push(0); // Use a default value of 0 for invalid ratings
-        } else {
-            slopeRatings.push(slopeRating);
-        }
-        teeNames.push(teeName);
-        }
+            teeNames.push(teeName);
+            courseRatings.push(isNaN(courseRating) ? 0 : courseRating);
+            slopeRatings.push(isNaN(slopeRating) ? 0 : slopeRating);
+        });
         // Create an array of objects with the extracted information
         const courseInfo = {
             Golfcourse_Name: golfCourseName,
             Course_Rating: teeNames.map((teeName, index) => ({ teeName, rating: courseRatings[index] })),
             Course_SlopeRating: teeNames.map((teeName, index) => ({ teeName, slopeRating: slopeRatings[index] })),
         };
-        // const courseNamesUSGA = courseInfo.Golfcourse_Name;
-        // console.log('All USGA course names:', courseNamesUSGA);
         return courseInfo;
 
     } catch (error) {
         console.error("Error processing HTML data for keyword:", keyword, error);
         return null;
     }    
-};
+}
+
 
 const golfCourseKeywords = [];
 let lineCount = 0; // variable to track the current line number
-    
-fs.createReadStream(csvFilePath)
+
+fileStream
     .pipe(csvParser())
     .on('data', (row) => {
         lineCount++;
-        if (lineCount < 397) return; // * Skip lines before 170 데이터 추가할때!!
+        if (lineCount < 464) return; // * Skip lines before 397 데이터 추가할때!!
 
-        const keyword = row.Keyword;
-        // Check if course does not exist in our Set
-        if (!existingCourseNames.has(keyword)) {
-            golfCourseKeywords.push(keyword);
+        const keyword = row.keyword; // Ensure 'keyword' matches the CSV header exactly
+        if (keyword && keyword.trim()) {
+            // Check if course does not exist in our Set
+            if (!existingCourseNames.has(keyword)) {
+                golfCourseKeywords.push(keyword);
+            }
         }
     })
     .on('end', async () => {  
+        if (golfCourseKeywords.length === 0) {
+            //console.log('No valid keywords found. Exiting.');
+            return;
+        }
         async function main() {
             for (const keyword of golfCourseKeywords) {
                 try {
@@ -219,79 +221,101 @@ fs.createReadStream(csvFilePath)
                         console.log(`No data returned for keyword: ${keyword}`);
                         continue; // skip if no course info returned
                     }
-
                     // Try to insert the data
                     try {
                         await insertGolfCourseData(courseInfo);
                     } catch (insertError) {
                         console.error(`Error inserting data for keyword "${keyword}":`, insertError);
                     }
-                    
                 } catch (error) {
                     console.error(`Error processing keyword "${keyword}":`, error);
                 }
             }
         }
         main();   
+    })
+    .on('error', (err) => {
+        console.error('Error reading CSV file:', err);
     });
-
-
-
-//Routing
-//API key
-
-const golfFinderApiKey = '234b53b3ccmshb82c7579628c603p1da094jsn811e7646403a'
-const apiKey = '40b6c60e-5176-4733-b670-0b3e539e1ae3';
-const courseId = '141520658891108829';
-const geocodingApiKey = 'AIzaSyDxp3pJu2sISavqnPqunkK5FgaTvpqkKdA';
-app.get('/search', async (req, res) => {
-    try {
-        const { lat, lng } = await getCoordinatesFromAddress(req.query.location);
-        const courseNames = await getNearbyGolfCourses(lat, lng);
-
-        // Fetch additional details from your DB
-        const detailedCourseInfo = await fetchGolfCourseDetails(courseNames);
-
-        console.log(detailedCourseInfo);
-
-        res.json(detailedCourseInfo);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).send('Error fetching data.');
-    }
-});
-
-async function getCoordinatesFromAddress(location) {
-    const geocodingUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
-    const response = await axios.get(geocodingUrl, { params: { address: location, key: geocodingApiKey } });
-    const results = response.data.results;
     
-    if (!results || results.length === 0) {
-        throw new Error('Location not found.');
-    }
 
-    return results[0].geometry.location;
-}
+    async function getCoordinatesFromAddress(location) {
+        const geocodingUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+        try {
+            const response = await axios.get(geocodingUrl, 
+            { params: { 
+                address: location, 
+                components: "country:US",  // Corrected typo from 'componets' to 'components'
+                key: geocodingApiKey
+            }});
+    
+            // Check if the API response is successful
+            if (response.data.status !== 'OK') {
+                throw new Error(`Location not found or API key issue. Status: ${response.data.status}`);
+            }
+    
+            // Extract the latitude and longitude from the first result
+            const { lat, lng } = response.data.results[0].geometry.location;
+            console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+            return { lat, lng };
+        } catch (error) {
+            console.error('Error fetching geocoding data:', error.message);
+            throw error;
+        }
+    }
+    
 
 async function getNearbyGolfCourses(lat, lng) {
-    const golfFinderUrl = 'https://golf-course-finder.p.rapidapi.com/courses';
-    const response = await axios.get(golfFinderUrl, {
-        params: { lat, lng, radius: 5 },
-        headers: {
-            'X-RapidAPI-Key': golfFinderApiKey,
-            'X-RapidAPI-Host': 'golf-course-finder.p.rapidapi.com'
-        },
-    });
+    const golfFinderUrl = 'https://golf-course-finder.p.rapidapi.com/api/golf-clubs/';
+    try {
+        // Validate the latitude and longitude
+        if (typeof lat !== 'number' || typeof lng !== 'number' || isNaN(lat) || isNaN(lng)) {
+            console.error('Invalid latitude or longitude:', lat, lng);
+            throw new Error('Invalid latitude or longitude provided.');
+        }
 
-    const courses = response.data.courses || [];
-    const courseNames = courses.map(course => course.name);
-    return courseNames;
-    console.log('Fetched course names from Golf Finder API:', courseNames);
+        // Perform the API request
+        const response = await axios.get(golfFinderUrl, {
+            params: { 
+                latitude: lat, 
+                longitude: lng, 
+                miles: 10 // Adjust the radius as needed
+            },
+            headers: {
+                'x-rapidapi-key': process.env.golfFinderApiKey,
+                'x-rapidapi-host': 'golf-course-finder.p.rapidapi.com'
+            }
+        });
+        
+        // Log the raw response data for debugging
+        //console.log('API Response:', response.data);
+
+        // Check the API response structure and extract golf club names
+        if (!response.data||response.data===0) {
+            console.log('No golf courses found or the API response structure is incorrect');
+            return [];
+        }
+
+        const clubName = response.data.map(club=>club.club_name); // Accessing nested golf_courses array
+        console.log('Fetched golf course names from Golf Finder API:', clubName);
+        return clubName;
+    } catch (error) {
+        console.error('Error fetching data from Golf Finder API:', error.response ? error.response.data : error.message);
+        throw error;
+    }
 }
 
+
+
 async function fetchGolfCourseDetails(courseNames) {
+    if (courseNames.length === 0) {
+        console.log("No course names to search for.");
+        return []; // Return an empty array if there are no course names
+    }
     try {
-        // SQL query to fetch details of golf courses that match with courseNames
+        const sanitizedCourseNames = courseNames.map(name => name.trim().toLowerCase());
+        console.log('Sanitized course names:', sanitizedCourseNames);
+        
         const query = `
             SELECT 
                 a.api_course_id,
@@ -309,23 +333,16 @@ async function fetchGolfCourseDetails(courseNames) {
             WHERE 
                 m.api_course_name IN (?);
         `;
-        const sanitizedCourseNames = courseNames.map(name => name.trim().toLowerCase());
-        console.log('Sanitized course names:', sanitizedCourseNames);
+        
         const [results] = await pool.query(query, [sanitizedCourseNames]);
-        // console.log('Query results:', results);
-        // const [results] = await pool.query(query, [courseNames]);
-
-
+        console.log('Fetched golf course details:', results);
+        
         return results;
     } catch (error) {
         console.error("Error fetching golf course details:", error);
         throw error;
     }
 }
-
-
-
-
 
 
 // Add an interceptor to catch and log errors globally
@@ -338,12 +355,31 @@ axios.interceptors.response.use(
 );
 
 
+//Routing
+app.get('/search', async (req, res) => {
+    try {
+        const location = req.query.location;
+        if (!location) {
+            console.error('Location query parameter is required.');
+            return res.status(400).send('Location query parameter is required.');
+        }
+        const { lat, lng } = await getCoordinatesFromAddress(req.query.location);
+        const courseNames = await getNearbyGolfCourses(lat, lng);
+        const detailedCourseInfo = await fetchGolfCourseDetails(courseNames);
+
+        res.json(detailedCourseInfo);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Error fetching data.');
+    }
+});
+
 
 app.get('/', function (req, res) {
-    res.render('index')  // ./views/index.ejs 불러와서 출력
+    res.render('index')  // ./views/index.ejs 불러와서 출력  Render the index.ejs view when the root URL is accessed
 })
 
-app.get('/ranking', function (req, res) {
+app.get('/ranking', function (req, res) { //// Render the ranking.ejs view when '/ranking' URL is accessed
     res.render('ranking')  
 })
 
@@ -367,11 +403,4 @@ app.get('/NorthernCali', function (req, res) {
     res.render('NorthernCali') 
 })
 
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
 app.listen(port)
-
